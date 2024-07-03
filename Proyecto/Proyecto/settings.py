18a2bb3203ed19ac31bec02ef8374ef2a922e9db
SoftwareSBGH <softwaresbfb@gmail.com>
2024-07-03 11:47:40 -0600

    Cambio de puerto

--- Proyecto/Proyecto/settings.py
+++ Proyecto/Proyecto/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('PASSWORD'),
         'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
+        'PORT': '1433',
         'OPTIONS': {
             'driver': 'ODBC Driver 17 for SQL Server',
         }
